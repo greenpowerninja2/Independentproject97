ec2989c1ab76c828d07898f9d03d93bffad0a1dd
greenpowerninja2 <118147535+greenpowerninja2@users.noreply.github.com>
2023-08-27 20:42:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function plane1()
+{
+window.location = "plane1.html";
+}
+function plane2()
+{
+window.location = "plane2.html";
+}
+function plane3()
+{
+window.location = "plane3.html";
+}
+function logout()
+{
+    window.location = "index.html";
+}
